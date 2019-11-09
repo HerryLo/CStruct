212d328ff0d1320c8af8a4d986ecaa5457bd209f
herrylo <herryloyopai@163.com>
2019-11-10 00:58:05 +0800

    add

--- list/main.c
+++ list/main.c
@@ -6,51 +6,13 @@
 //  Copyright © 2019年 HerryLo. All rights reserved.
 //
 
-// 线性结构：连续存储 数组
-
+// 线性结构：离散存储 链表
 #include <stdio.h>
-
-#include "ArrList/ArrList.h"
-
-typedef struct ArrList {
-    int *p;     //  数组第一个元素的地址
-    int len;    //  数组长度
-    int cnt;    //  数组元素的有效长度
-}ArrList; //别名 ArrList
-
-// 初始化创建数组
-void arrInit(ArrList *arr, int length);
-// 添加(尾部)
-void append(ArrList);
-// 插入
-void insert(ArrList);
-// 删除
-void delet(ArrList);
-// 排序
-void sort(ArrList);
-
-// 判断是否空
-void isEmpty(ArrList);
-// 判断是否满
-void isFullF(ArrList);
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
-    
-    ArrList arr;
-    arrInit(&arr, 6);
-    
-    printf("%d\n", arr.len);
+    printf("数组");
     
     return 0;
-}
-
-
-void arrInit(ArrList *arr, int length) {
-    arr->p = (int *)malloc(sizeof(int) * length);
-    
-    if(arr == NULL)
-        printf("Array create fail");
-    else
-        arr->len = length;
-    arr->cnt = 0;
 }
