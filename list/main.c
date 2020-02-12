dc82ffe5d965cd8e7ba2cc2c87de559c45976b87
herrylo <herryloyopai@163.com>
2020-02-12 16:45:10 +0800

    set

--- list/main.c
+++ list/main.c
@@ -11,18 +11,49 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+// 结构体
 typedef struct Node {
     int data; // 数据域
     struct Node * next; //指针域
 }Node, *PNode;
 
-PNode createList() {
+//  创建非循环单链表
+PNode createList(void) {
+    // 长度
+    int len;
+    // 存放用户输入的节点值
+    int val;
     
-};
+    PNode pHead = (PNode)malloc(sizeof(Node));
+    if(NULL == pHead) {
+        printf("分配失败，程序终止！\n");
+        exit(-1);
+    }
+    
+    printf("请输入链表的长度: len=");
+    scanf("%d", &len);
+    
+    for (int i = 0; i<len; ++i) {
+        printf("请输入第%d个节点的值: ", i+1);
+        scanf("%d", &val);
+        
+        PNode pNew = (PNode)malloc(sizeof(Node));
+        if(NULL == pNew) {
+            printf("分配失败，程序终止！\n");
+            exit(-1);
+        }
+        
+        
+
+    }
+    
+    return pHead;
+}
 
 int main() {
     // 头节点
     PNode pHead = NULL; // 等价于struct Node * element = NULL
-
+    
+    pHead = createList();
     return 0;
 }
